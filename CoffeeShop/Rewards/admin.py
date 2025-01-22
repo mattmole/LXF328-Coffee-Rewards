f41b324fe59018a4260ce372da1bf9c74221eeaf
Matt Holder <mattmole@gmail.com>
2025-01-22 10:06:06 +0000

    Small tweaks

--- CoffeeShop/Rewards/admin.py
+++ CoffeeShop/Rewards/admin.py
@@ -6,7 +6,7 @@ class AccountAdmin(admin.ModelAdmin):
     list_display = ('accountCode', 'dateTimeCreated', 'currentPoints', 'totalPoints', 'availableRewards', 'rewardsUsed')
 
 class AccountOperationAdmin(admin.ModelAdmin):
-    list_display = ('account__accountCode', 'dateTimeOfOperation', 'operation', 'pointsChange')
+    list_display = ('account', 'dateTimeOfOperation', 'operation', 'pointsChange')
 
 class AccountCodeBuilderAdmin(admin.ModelAdmin):
     list_display = ('word',)
